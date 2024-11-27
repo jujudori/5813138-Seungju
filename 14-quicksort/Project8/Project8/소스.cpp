5a079ec9a2f631f5b4ddc34be9c491ffec96642c
jujudori <seung12397@naver.com>
2024-11-27 15:20:18 +0900

    Add files via upload

---
+++ 14-quicksort/Project8/Project8/소스.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout.put('H');
+	cout.put('i');
+	cout.put(33);
+	cout.put('\n');
+
+	cout.put('c').put('+').put('+').put(' ');
+
+	char str[] = "i love programing";
+	cout.write(str, 6);
+
+}
